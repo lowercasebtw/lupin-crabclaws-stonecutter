4ef275326a17836c8bdbc65d73214eba4eb84792
lowercasebtw <ignzoudy@gmail.com>
2025-11-21 17:14:02 -0500

    Add temporary crash fix for 1.21.10 neoforge

---
+++ src/main/java/dev/foxgirl/crabclaws/mixins/MixinPreparableReloadListenerSharedState.java
@@ -0,0 +1,23 @@
+package dev.foxgirl.crabclaws.mixins;
+// TODO/NOTE: Temporary fix to get 1.21.10 neoforge to load and not crash immediately
+
+import net.minecraft.server.packs.resources.PreparableReloadListener;
+import org.spongepowered.asm.mixin.Mixin;
+
+//? >=1.21.10 && neoforge {
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import org.spongepowered.asm.mixin.injection.At;
+//? }
+
+//? >=1.21.10 && neoforge {
+@Mixin(PreparableReloadListener.SharedState.class)
+//? }
+public abstract class MixinPreparableReloadListenerSharedState {
+    //? >=1.21.10 && neoforge {
+    @WrapOperation(method = "get", at = @At(value = "INVOKE", target = "Ljava/util/Objects;requireNonNull(Ljava/lang/Object;)Ljava/lang/Object;"))
+    private <T> T crabclaws$fixArchitectury(T object, Operation<T> original) {
+        return object;
+    }
+    //? }
+}
